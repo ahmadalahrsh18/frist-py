7ee7d85fcc0ee7fef5d590134f6dda54ff7d2594
ahmadalahrsh18 <110725000+ahmadalahrsh18@users.noreply.github.com>
2023-02-12 17:39:55 +0200

    Add files via upload

---
+++ project.py
@@ -0,0 +1,18 @@
+def plotImages(images_arr, probabilities = False):
+    fig, axes = plt.subplots(len(images_arr), 1, figsize=(5,len(images_arr) * 3))
+    if probabilities is False:
+      for img, ax in zip( images_arr, axes):
+          ax.imshow(img)
+          ax.axis('off')
+    else:
+      for img, probability, ax in zip( images_arr, probabilities, axes):
+          ax.imshow(img)
+          ax.axis('off')
+          if probability > 0.5:
+              ax.set_title("%.2f" % (probability*100) + "% dog")
+          else:
+              ax.set_title("%.2f" % ((1-probability)*100) + "% cat")
+    plt.show()
+
+sample_training_images, _ = next(train_data_gen)
+plotImages(sample_training_images[:5])
